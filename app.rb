9b0e2902b89e51b23c7b2c4c5e0c4af0e18c46a7
Rick Denman <denman@southwestern.edu>
2015-12-20 17:41:21 +0000

    modifying new.erb

--- app.rb
+++ app.rb
@@ -57,17 +57,29 @@ class HangpersonApp < Sinatra::Base
   # wrong_guesses and word_with_guesses from @game.
   get '/show' do
     ### YOUR CODE HERE ###
-    erb :show # You may change/remove this line
+    result = @game.check_win_or_lose
+    if result == :win
+      flash[:message] = "You win!"
+      redirect '/win'
+    elsif result == :lose
+      flash[:message] = "You lose (7 tries)!!"
+      redirect '/lose'
+    else
+      redirect '/guess'
+    end
+    #erb :show # You may change/remove this line
   end
   
   get '/win' do
     ### YOUR CODE HERE ###
+    redirect '/create'
     erb :win # You may change/remove this line
   end
   
   get '/lose' do
     ### YOUR CODE HERE ###
-    erb :lose # You may change/remove this line
+    redirect '/create'
+    #erb :lose # You may change/remove this line
   end
   
 end
